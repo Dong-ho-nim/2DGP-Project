960e7c44f8eccbd9911e528e9218b317c4bc7ec2
Dong-ho-nim <shindh758@gmail.com>
2025-10-17 22:51:48 +0900

    handle_input 만들기

--- game_framework.py
+++ game_framework.py
@@ -9,4 +9,27 @@ class Fighter:
         self.attack_power = 10
         self.is_attacking = False
 
+    def handle_input(self, keys, left=True):
+        if left:
+            if keys[pygame.K_a]:
+                self.rect.x -= self.speed
+            if keys[pygame.K_d]:
+                self.rect.x += self.speed
+            if keys[pygame.K_w]:
+                self.rect.y -= self.speed
+            if keys[pygame.K_s]:
+                self.rect.y += self.speed
+            if keys[pygame.K_SPACE]:
+                self.is_attacking = True
+        else:
+            if keys[pygame.K_LEFT]:
+                self.rect.x -= self.speed
+            if keys[pygame.K_RIGHT]:
+                self.rect.x += self.speed
+            if keys[pygame.K_UP]:
+                self.rect.y -= self.speed
+            if keys[pygame.K_DOWN]:
+                self.rect.y += self.speed
+            if keys[pygame.K_RETURN]:
+                self.is_attacking = True
 
