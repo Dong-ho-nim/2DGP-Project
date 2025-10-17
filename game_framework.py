0c68a9b5f5159b1d9d24aa67bf5758e085358b59
Dong-ho-nim <shindh758@gmail.com>
2025-10-17 22:50:46 +0900

    class Fighter 만들기

--- game_framework.py
+++ game_framework.py
@@ -1,1 +1,12 @@
+from pico2d import *
+
+class Fighter:
+    def __init__(self, x, y, color):
+        self.rect = pygame.Rect(x, y, 50, 100)  # 캐릭터 크기
+        self.color = color
+        self.hp = 100
+        self.speed = 5
+        self.attack_power = 10
+        self.is_attacking = False
+
 
