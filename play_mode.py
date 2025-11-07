f50a1e933eec54139f8d9117de447117e68cab17
Dong-ho-nim <shindh758@gmail.com>
2025-11-07 23:02:03 +0900

    배경추가

--- play_mode.py
+++ play_mode.py
@@ -4,19 +4,16 @@ from BackGround import BackGround
 
 character = None
 background = None
-sky = None
 
 def enter():
-    global character, background, sky
+    global character, background
     background = BackGround()
-    sky = Sky()
     character = Byakuya()
 
 def exit():
-    global character, background, sky
+    global character, background
     character = None
     background = None
-    sky = None
 
 def handle_event(event):
     if event.type == SDL_QUIT:
@@ -26,8 +23,6 @@ def handle_event(event):
 def update():
     if background:
         background.update()
-    if sky:
-        sky.update()
     if character:
         character.update()
 
@@ -35,8 +30,6 @@ def draw():
     clear_canvas()
     if background:
         background.draw()
-    if sky:
-        sky.draw()
     if character:
         character.draw()
     update_canvas()
