ed99f12732ab8e3e876ed61a9ab1d1564be360dc
Dong-ho-nim <shindh758@gmail.com>
2025-11-07 22:38:50 +0900

    애니메이션 그려내기위해 코드 수정

--- play_mode.py
+++ play_mode.py
@@ -1,4 +1,4 @@
-import pico2d
+from pico2d import *
 from Player import Byakuya
 
 character = None
@@ -11,7 +11,7 @@ def exit():
     pass
 
 def handle_event(event):
-    if event.type == pico2d.SDL_QUIT:
+    if event.type == SDL_QUIT:
         import game_framework
         game_framework.quit()
 
@@ -19,6 +19,6 @@ def update():
     character.update()
 
 def draw():
-    pico2d.clear_canvas()
+    clear_canvas()
     character.draw()
-    pico2d.update_canvas()
+    update_canvas()
