602742364589ac4655569cd74dd783d86ed6f32f
Dong-ho-nim <shindh758@gmail.com>
2025-12-01 02:08:49 +0900

    배경 수정

--- BackGround.py
+++ BackGround.py
@@ -1,9 +1,26 @@
 # BackGround.py
-from pico2d import load_image
+from pico2d import load_image, get_canvas_width, get_canvas_height
 
 class BackGround:
     def __init__(self):
-        self.image = load_image('background.png')   # 프로젝트 루트에 배경 넣기
-    def update(self): pass
+        self.sky_image = load_image('Sky.png')
+        self.ground_image = load_image('BackGround.png')
+        self.canvas_width = get_canvas_width()
+        self.canvas_height = get_canvas_height()
+
+    def update(self):
+        pass
+
     def draw(self):
-        self.image.draw(600, 350)   # 1200×700 캔버스 중앙
+        # Draw sky image, scaling it to fill the canvas
+        self.sky_image.clip_draw(
+            0, 0, self.sky_image.w, self.sky_image.h,
+            self.canvas_width // 2, self.canvas_height // 2,
+            self.canvas_width, self.canvas_height
+        )
+        # Draw ground image, scaling it to fill the canvas
+        self.ground_image.clip_draw(
+            0, 0, self.ground_image.w, self.ground_image.h,
+            self.canvas_width // 2, self.canvas_height // 2,
+            self.canvas_width, self.canvas_height
+        )
